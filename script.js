30723895d8abed07a731353b9202329a115791bd
Samuel Heitor <115489905+Astr0ns@users.noreply.github.com>
2023-02-24 17:19:40 +0000

    Update portifólio

--- script.js
+++ script.js
@@ -1,39 +1,22 @@
-function replacemain1() {
+function substituir1() {
 
-    var main1 = window.document.getElementById('main')
-    var main2 = window.document.getElementById('text-main')
-  
-    main2.style.display = "block";
-    main1.style.display = "none";
-  }
-  
-  function replacemain2() {
-  
-    var menu1 = window.document.getElementById('main')
-    var menu2 = window.document.getElementById('text-main')
-  
-    menu1.style.display = "block";
-    menu2.style.display = "none";
-  
-  }
-  
-  function replacemenu3() {
-  
-    var menu2 = window.document.getElementById('text-main')
-    var menu3 = window.document.getElementById('proj-div')
-  
-    menu2.style.display = "block"
-    menu3.style.display = "none";
-  
-  }
-  
-  function replacemenu() {
-  
-    var menu2 = window.document.getElementById('text-main')
-    var menu3 = window.document.getElementById('proj-div')
-  
-    menu2.style.display = "block";
-    menu3.style.display = "none";
-  
-  }
+  var main1 = window.document.getElementById('main1')
+  var main2 = window.document.getElementById('main2')
+
+  main2.style.display = "block";
+  main1.style.display = "none";
+}
+function substituir2() {
+var main1 = window.document.getElementById('main2')
+var main2 = window.document.getElementById('main1')
+
+main2.style.display = "block";
+main1.style.display = "none";
+}
+function substituir3(){
+  var main2 = window.document.getElementById('main3')
+  var main3 = window.document.getElementById('main2')
+
+  main2.style.display = "block";
+  main3.style.display = "none";
 }
